0e067a9e246091c0715c2357548db6be09c85aeb
majmehdi <majmehdi20@gmail.com>
2025-02-09 17:10:08 +0100

    I've fixed the patient class

--- App/Models/Patient.php
+++ App/Models/Patient.php
@@ -1,5 +1,4 @@
 <?php
-// File: /app/models/Patient.php
 require_once __DIR__ . '/../../core/Database.php';
 
 class Patient {
@@ -9,14 +8,12 @@ class Patient {
         $this->db = new Database();
     }
     
-    // Retrieve the patient's record using the user_id from public.patients.
     public function getPatientByUserId($user_id) {
         $this->db->query("SELECT * FROM public.patients WHERE user_id = :user_id");
         $this->db->bind(':user_id', $user_id);
         return $this->db->fetch();
     }
     
-    // Get all appointments for the patient.
     public function getAppointments($patient_id) {
         $this->db->query("
             SELECT 
@@ -34,7 +31,6 @@ class Patient {
         return $this->db->fetchAll();
     }
     
-    // Retrieve a list of all doctors for booking a new appointment.
     public function getDoctorList() {
         $this->db->query("
             SELECT d.*, u.first_name AS user_first, u.last_name AS user_last, u.email
@@ -45,8 +41,6 @@ class Patient {
         return $this->db->fetchAll();
     }
     
-    // Insert a new rendez-vous (appointment) for the patient.
-    // The new appointment will have status 'in progress' by default.
     public function createAppointment($patient_id, $doctor_id, $appointment_date, $reason) {
         $this->db->query("
             INSERT INTO public.rendez_vous (patient_id, doctor_id, appointment_date, reason)
